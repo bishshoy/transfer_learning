201837c7254da7c72365121d0bb91743f8178031
Bishshoy Das <bishshoydas@gmail.com>
2022-04-21 22:36:28 +0000

    Update parser defaults.

--- parsers.py
+++ parsers.py
@@ -7,11 +7,11 @@ def get_args():
     parser.add_argument('--pretrained', action='store_true')
     parser.add_argument('--print-model', action='store_true')
 
-    parser.add_argument('--batch-size', type=int, default=256)
+    parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--epochs', type=int)
     parser.add_argument('--continuous', type=int, default=5)
 
-    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--lr', type=float, default=1e-2)
     parser.add_argument('--mom', type=float, default=0.9)
     parser.add_argument('--wd', type=float, default=5e-4)
 
